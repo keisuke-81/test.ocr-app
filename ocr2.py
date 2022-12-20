1771ad6f98bdba295aac746d642796c2f6218744
keisuke-81 <sakura-jima@live.jp>
2022-12-20 15:27:28 +0900

    文字を少し変えて画像も入れました

--- ocr2.py
+++ ocr2.py
@@ -5,16 +5,16 @@ import numpy as np #Image Processing
 
 
 #title
-st.title("OCRアプリ - 画像からテキスト抽出")
+st.title("OCRアプリ  画像からテキスト抽出")
 
 #subtitle
-st.markdown("## 光学式文字認識 - 使用 `easyocr`, `streamlit`")
+st.markdown("## 光学式文字認識 - 使用 `英語、日本語対応`")
 
 st.markdown("")
 
 #写真をアップロード
 image = st.file_uploader(label = "ここへドラグアンドドロップもしくはクリックしてファイル選択で画像をアップロード",type=['png','jpg','jpeg'])
-
+image_dog = Image.open('dog.png')
 
 @st.cache
 def load_model(): #ocr読み取りの
@@ -47,11 +47,11 @@ if image is not None:
         result = reader.readtext(np.array(input_image)) #抽出したテキストをnp.arrayによってnumpyの配列に変換(Supporting format = string(file path or url), bytes, numpy array)
         text_word = np.stack(result) #numpyの配列をstrackによって結合して表に変換する
         st.markdown("## 🌟　`読み取り結果`　🌟 ")
-        st.write(text_word[:,1]) #表の中で文章が表示されている列のみを([:,1])表示させる
+        st.write(text_word[:,1],width=700) #表の中で文章が表示されている列のみを([:,1])表示させる
         st.snow() #完了した時に雪を降らせる
 else:
-    st.write("画像をアップロードしてください")
-
+    st.write("画像をアップロードしてください(200MBまで)")
+    st.image(image_dog,width=280)
 #st.caption("Made with ❤️ by @1littlecoder")
 
 
