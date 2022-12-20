57832bd06d181cc8c7284f9c08f1f38e619c1ac8
keisuke-81 <sakura-jima@live.jp>
2022-12-20 14:31:12 +0900

    second

--- ocr2.py
+++ ocr2.py
@@ -3,6 +3,7 @@ import streamlit as st  #Web App
 from PIL import Image #Image Processing
 import numpy as np #Image Processing 
 
+
 #title
 st.title("OCRアプリ - 画像からテキスト抽出")
 
@@ -16,33 +17,40 @@ image = st.file_uploader(label = "ここへドラグアンドドロップもし�
 
 
 @st.cache
-def load_model(): 
+def load_model(): #ocr読み取りの
     reader = ocr.Reader(['en','ja'],model_storage_directory='.')
     return reader 
 
-reader = load_model() #load model
+reader = load_model() #load model 空っぽの入れ物にデータを入れる　インスタンス化　変数変えても
 
 if image is not None:
 
-    input_image = Image.open(image) #read image
-    st.image(input_image) #display image
+    input_image = Image.open(image) #read image 画像の読み込み
+    st.image(input_image) #display image 画像の表示
+    # 進捗表示用のプレースホルダー
+    status_text = st.empty()
+    # プログレスバー
+    # progress_bar = st.progress(0)
+
+    # for i in range(100):
+    #     # 進捗表示の文字列を更新
+    #     status_text.text(f'Progress: {i}%')
+    #     # for ループ内でプログレスバーの状態を更新する
+    #     progress_bar.progress(i + 1)
+    #     sleep(0.1)
+    
 
     with st.spinner("ただいま画像読み取り中! "):
         
-
-        result = reader.readtext(np.array(input_image))
-
-        result_text = [] #empty list for results
-
-
-        for text in result:
-            result_text.append(text[1])
-
-        st.write(result_text)
-    #st.success("Here you go!")
-    st.balloons()
+        
+        #ocr処理
+        result = reader.readtext(np.array(input_image)) #抽出したテキストをnp.arrayによってnumpyの配列に変換(Supporting format = string(file path or url), bytes, numpy array)
+        text_word = np.stack(result) #numpyの配列をstrackによって結合して表に変換する
+        st.markdown("## 🌟　`読み取り結果`　🌟 ")
+        st.write(text_word[:,1]) #表の中で文章が表示されている列のみを([:,1])表示させる
+        st.snow() #完了した時に雪を降らせる
 else:
-    st.write("Upload an Image")
+    st.write("画像をアップロードしてください")
 
 #st.caption("Made with ❤️ by @1littlecoder")
 
